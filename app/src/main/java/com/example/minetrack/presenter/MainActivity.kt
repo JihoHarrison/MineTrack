c69df15e1e93b9f17bb0d8ab44c40eea64eeacc4
JihoKevin <jhshin925@gmail.com>
2021-11-16 19:43:47 +0900

    Fix MainActivity

--- app/src/main/java/com/example/minetrack/presenter/MainActivity.kt
+++ app/src/main/java/com/example/minetrack/presenter/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.minetrack.presenter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.minetrack.R
+import com.example.minetrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        val navigationController =
+            (supportFragmentManager.findFragmentById(R.id.mainNavigationHostContainer) as NavHostFragment).navController
+        binding.toolbar.setupWithNavController(navigationController)
     }
 }
